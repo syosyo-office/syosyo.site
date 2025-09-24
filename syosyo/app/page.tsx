1a9312a8b4b2f8d264937c02cff539c7432346f5
“meio” <meio134340@gmail.com>
2025-09-24 22:16:52 +0900

    12

--- syosyo/app/page.tsx
+++ syosyo/app/page.tsx
@@ -5,18 +5,35 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="page-top">
-      <div className="link-contents fade-in">
-        <a className="link-txt" href="https://syosyo.jp" target="_blank" rel="noopener noreferrer">しょしょのサイト(syosyo.jp)</a>
-        <p>ドメインだけ保有中。何もまだ開発してません。</p>
+      <div className="link-contents">
+        <a className="link-txt" href="https://syosyo.jp" target="_blank" rel="noopener noreferrer">
+        <Image
+                    className="syosyo-thumbnail fade-in"
+                    src="/img/syosyo.jp.png"
+                    alt="syosyo.jp site"
+                    width={5140}
+                    height={3132}
+                    priority
+                  />
+        </a>
       </div>
-      <div className="link-contents fade-in">
-        <a className="link-txt" href="https://scalloped-chartreuse-5bd.notion.site/219c75973b4880e68b22d787fa2b7ca3?source=copy_link" target="_blank" rel="noopener noreferrer">しょしょの制作過程(notion)</a>
+      <div className="link-contents">
+        <a className="link-txt" href="https://scalloped-chartreuse-5bd.notion.site/219c75973b4880e68b22d787fa2b7ca3?source=copy_link" target="_blank" rel="noopener noreferrer">
+        <Image
+                    className="syosyo-thumbnail fade-in"
+                    src="/img/syosyo-notion.png"
+                    alt="syosyo-notion site"
+                    width={5140}
+                    height={3132}
+                    priority
+                  />
+        </a>
         
       </div>
-      <div className="link-contents fade-in">
+      <div className="link-contents">
         <a className="link-txt" href="https://syosyo.info" target="_blank" rel="noopener noreferrer">
         <Image
-                    className="syosyo-info"
+                    className="syosyo-thumbnail fade-in"
                     src="/img/syosyo.info.png"
                     alt="syosyo.info site"
                     width={5140}
@@ -25,11 +42,29 @@ export default function Home() {
                   />
         </a>
       </div>
-      <div className="link-contents fade-in">
-        <a className="link-txt" href="https://note.com/syosyo_" target="_blank" rel="noopener noreferrer">しょしょのnote</a>
+      <div className="link-contents">
+        <a className="link-txt" href="https://note.com/syosyo_" target="_blank" rel="noopener noreferrer">
+        <Image
+                    className="syosyo-thumbnail fade-in"
+                    src="/img/syosyo-note.png"
+                    alt="syosyo-note site"
+                    width={5140}
+                    height={3132}
+                    priority
+                  />
+        </a>
       </div>
-      <div className="link-contents fade-in">
-        <a className="link-txt" href="https://www.instagram.com/syosyo.office/" target="_blank" rel="noopener noreferrer">しょしょのインスタ</a>
+      <div className="link-contents">
+        <a className="link-txt" href="https://www.instagram.com/syosyo.office/" target="_blank" rel="noopener noreferrer">
+        <Image
+                    className="syosyo-thumbnail fade-in"
+                    src="/img/syosyo-instagram.png"
+                    alt="syosyo-instagram site"
+                    width={5140}
+                    height={3132}
+                    priority
+                  />
+        </a>
       </div>
       <footer>
         <a className="link-txt" href="mailto:syosyo.office@gmail.com">syosyo.office@gmail.com</a>
